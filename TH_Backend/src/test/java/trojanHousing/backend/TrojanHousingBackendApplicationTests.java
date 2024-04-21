0ea2413aed75448f7965700019d1ac62eb7f6e93
Nicolas Peterson <nrpeters@usc.edu>
2024-04-20 17:00:56 -0700

    initial push of backend

---
+++ TH_Backend/src/test/java/trojanHousing/backend/TrojanHousingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package trojanHousing.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrojanHousingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
