6b9686cfac35fa0b2f51f526cc4a704bcfb616bd
Bridgett O'Grady <bogrady@usc.edu>
2024-04-24 00:37:53 -0700

    adding property and image controller

--- TH_Backend/src/main/java/trojanHousing/backend/controller/ImageController.java
+++ TH_Backend/src/main/java/trojanHousing/backend/controller/ImageController.java
@@ -1,5 +1,21 @@
 package trojanHousing.backend.controller;
 
-public class ImageController {
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
+@Controller
+public class ImageController {
+	/*getImage(PropertyID) this gets called from our Listing class and returns an image URL.*/
+	
+	@Autowired
+	ImageRepository imageRepository;
+	
+	@RequestMapping(value = "/getImage", method = RequestMethod.GET)
+	public String getImage(@RequestParam("property_id") int pid) {
+		return imageRepository.getImage(pid);
+	}
 }
--- TH_Backend/src/main/java/trojanHousing/backend/controller/PropertyController.java
+++ TH_Backend/src/main/java/trojanHousing/backend/controller/PropertyController.java
@@ -9,11 +9,31 @@ import java.util.List;
 
 @Controller
 public class PropertyController {
+	
+	/* searchListingsByFilter(Filter filter): This will use the applied filters within the Filter   
+	 * object input parameter to return a list of all properties that match the applied filters. This will also call the Image class to get Image URLs.
+	 */
+	
 	/* getSavedListings(int uid): This will take an integer representing the User ID and call 
 	 * getSavedListings(uid) in the ListingReposity class, returning a list of Listing objects which have been saved by the user.
 	 */
-	@RequestMapping(value = "/editfooditem", method = RequestMethod.POST)
+	
+	@Autowired
+	PropertyRepository propertyRepository;
+	
+	@RequestMapping(value = "/getSavedProperties", method = RequestMethod.GET)
 	public List<Property> getSavedProperties(@RequestParam("user_id") int uid) {
-		
+		return propertyRepository.getSavedProperties(uid);
 	}
+	
+	/* addSavedListing(int uid, int pid): This will take a User ID and Property ID as its parameters and call 
+	 * addSavedListing() in the JDBC Connector class, returning -1 if it was unsuccessful or 0 if it was.
+	 */
+	
+	@RequestMapping(value = "/addSavedProperty", method = RequestMethod.GET)
+	public int addSavedListing(@RequestParam("user_id") int uid, @RequestParam("property_id") int pid) {
+		return propertyRepository.addSavedProperty(uid, pid);
+	}
+	
+	
 }
