47cab9137e036112a5ca4127039c833a40649894
Bridgett O'Grady <bogrady@usc.edu>
2024-04-26 19:45:20 -0700

    hopefully working image stuff

--- TH_Backend/src/main/java/trojanHousing/backend/controller/ImageController.java
+++ TH_Backend/src/main/java/trojanHousing/backend/controller/ImageController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import trojanHousing.backend.entity.Image;
 import trojanHousing.backend.entity.User;
+import trojanHousing.backend.repository.ImageRepository;
 import trojanHousing.backend.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,25 +23,19 @@ public class ImageController {
 	/*getImage(PropertyID) this gets called from our Listing class and returns an image URL.*/
 	
 	@Autowired
-	ImageRepository imageRepository;
+	ImageRepository imageRepo;
 	
 	@RequestMapping(value = "/getImage", method = RequestMethod.POST)
-	@RequestBody
-	public String getImages(@RequestParam("property_id") int pid) {
-		return imageRepository.getImage(pid);
-	}
-	
-	
-	/* @Controller 
-public class UserController {
-	@Autowired
-	UserRepository userRepo;
-	
-	@RequestMapping(value = "/userLogin", method = RequestMethod.POST)
 	@ResponseBody
-	public String login(@RequestParam("email") String email,
-			@RequestParam("password") String password, HttpServletResponse response, HttpSession session)
-			throws IOException {
-		try {
-		*/
+	public String getImages(@RequestParam("property_id") int pid) {
+		List<Image> images = imageRepo.getImages(pid);
+		List<String> urls = new ArrayList<>();
+		for (Image image: images) {
+			urls.add(image.getImageURL());
+		}
+		
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(urls);
+        return jsonString;
+	}
 }
