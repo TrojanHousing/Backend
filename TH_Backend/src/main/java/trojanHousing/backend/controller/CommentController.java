b102158cdac8ca5101feef4a63476272912a2280
Bridgett O'Grady <bogrady@usc.edu>
2024-04-28 11:54:52 -0700

    all comment functions work. test in imageComment.html

--- TH_Backend/src/main/java/trojanHousing/backend/controller/CommentController.java
+++ TH_Backend/src/main/java/trojanHousing/backend/controller/CommentController.java
@@ -31,10 +31,8 @@ public class CommentController {
 	public String getCommentsByPropertyID(@RequestParam("propertyID") int propertyID,
 		HttpServletResponse response)
 		throws IOException {
-		System.out.println("in function. pid=" + propertyID);
 		try {
 			List<Comment> comments = commentRepo.getByProperty(propertyID);
-			System.out.println("gotten from repo. " + comments);
 			List<String> commentText = new ArrayList<>();
 			List<Integer> commentRatings = new ArrayList<>();
 			for (Comment comment: comments) {
@@ -43,14 +41,19 @@ public class CommentController {
 			}
 			// manually make json string
 			// {"name":"John","age":30,"email":"john@example.com"}
-			String commentsJson = "[";
+			String commentsJson = "";
+			if (comments.size() != 1) {
+				commentsJson += "[";
+			}
 			for (int i = 0; i < comments.size(); i++) {
 				commentsJson += "{\"text\":\"" + commentText.get(i) + "\",\"rating\":" + commentRatings.get(i) + "}";
 				if ((i + 1) != comments.size()) { // not last thing
 					commentsJson += ",";
 				}
 				else {
-					commentsJson += "]";
+					if (comments.size() != 1) {
+						commentsJson += "]";
+					}
 				}
 			}
 //			Gson gson = new Gson();
@@ -71,9 +74,30 @@ public class CommentController {
 		throws IOException {
 		try {
 			List<Comment> comments = commentRepo.getByUser(userID);
-			Gson gson = new Gson();
+			List<String> commentText = new ArrayList<>();
+			List<Integer> commentRatings = new ArrayList<>();
+			for (Comment comment: comments) {
+				commentText.add(comment.getText());
+				commentRatings.add(comment.getRating());
+			}
+			// manually make json string
+			String commentsJson = "";
+			if (comments.size() != 1) {
+				commentsJson += "[";
+			}
+			for (int i = 0; i < comments.size(); i++) {
+				commentsJson += "{\"text\":\"" + commentText.get(i) + "\",\"rating\":" + commentRatings.get(i) + "}";
+				if ((i + 1) != comments.size()) { // not last thing
+					commentsJson += ",";
+				}
+				else {
+					if (comments.size() != 1) {
+						commentsJson += "]";
+					}
+				}
+			}
 			response.setStatus(HttpServletResponse.SC_OK);
-			return gson.toJson(comments);
+			return commentsJson;
 		} catch (Exception e) {
 			System.out.println("Error occured at CommentsController : " + e.getMessage());
 			response.setStatus(404);
